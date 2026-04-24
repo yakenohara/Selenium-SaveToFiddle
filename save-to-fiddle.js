3348edc28c7cc2c9a420d6b047d9ddf7c252723a
yakenohara <38835246+yakenohara@users.noreply.github.com>
2026-04-24 22:21:33 +0900

    func_safeClipboardWrite 内で捨て要素を作成して ctrl+v して確認(途中)

--- save-to-fiddle.js
+++ save-to-fiddle.js
@@ -26,9 +26,13 @@ var obj_webDriver;
 
 (async function(){
 
-    for(var int_idxOfArgs = 2 ; int_idxOfArgs < process.argv.length; int_idxOfArgs++){
-        let str_arg = process.argv[int_idxOfArgs];
-        console.log(`Processing ${int_idxOfArgs-1}of${(process.argv.length-2)} "${str_arg}"`);
+    // for(var int_idxOfArgs = 2 ; int_idxOfArgs < process.argv.length; int_idxOfArgs++){
+    //     let str_arg = process.argv[int_idxOfArgs];
+    //     console.log(`Processing ${int_idxOfArgs-1}of${(process.argv.length-2)} "${str_arg}"`);
+    const strarr_filePath_in = fs.readFileSync(process.argv[2], 'utf-8').split('\r\n');
+    for(var int_idxOfArgs = 0 ; int_idxOfArgs < strarr_filePath_in.length; int_idxOfArgs++){
+        let str_arg = strarr_filePath_in[int_idxOfArgs];
+        console.log(`Processing ${int_idxOfArgs+1}of${(strarr_filePath_in.length)} "${str_arg}"`);
     
         var obj_result = await (async function(){
     
@@ -201,13 +205,12 @@ var obj_webDriver;
 
             objarr_elements = await fnc_a();
 
-            var str_urlDirNameL1 = afterUrl.replace(beforeUrl, '');
-            str_urlDirNameL1 = str_urlDirNameL1.replace(/(.+)\//, '$1');
+            var str_htmlContent = afterUrl.replace(beforeUrl, '');
+            str_htmlContent = str_htmlContent.replace(/(.+)\//, '$1');
+            str_htmlContent = `<div>${str_htmlContent}<div>`
             
             // Clip text for HTML
-            await func_safeClipboardWrite(
-                `<div>` + str_urlDirNameL1 + `<div>`
-            );
+            await func_safeClipboardWrite(str_htmlContent);
             
 
             // Click HTML Editor
@@ -480,16 +483,216 @@ var obj_webDriver;
             // クリップボードへの書き込みをリトライ付きで実行する
             //
             async function func_safeClipboardWrite(str_text, int_numOfRetries = 60) {
+
+                // OS ClipBoard に貼り付け
                 for (let int_i = 0; int_i < int_numOfRetries; int_i++) {
                     try {
                         clipboardy.writeSync(str_text);
-                        return; // 成功したら終了
+                        
+                        // 2. 読み取って確認 (Verification)
+                        let str_currentClipboard = clipboardy.readSync();
+                        if (str_currentClipboard === str_text) {
+                            // 内容が一致すれば成功
+                            break;
+                        } else {
+                            // 書き込みエラーは出ていないが、中身が一致しない場合もリトライへ
+                            console.warn(`Clipboard mismatch. Retrying... (${int_i + 1})`);
+                        }
                     } catch (e) {
                         if (int_i === int_numOfRetries - 1) throw e; // 最後のリトライでもダメならエラーを投げる
                         console.warn(`Clipboard copy failed. Retrying...`);
                         await new Promise(resolve => setTimeout(resolve, 200)); // 200ms 待機
                     }
                 }
+
+                // <ClipBoard 文字列を Selenium が意図通り引っ張ってこれるかどうか確認>--------------------------------------------------------
+                var str_id_tmp = `${func_checkclipboard.name}_0`;
+                while(true){ // 存在しない id 名称を抽出するまで無限ループ
+                    var obj_elems_expectedAs0 = await obj_webDriver.findElements(By.id(str_id_tmp));
+                    if(obj_elems_expectedAs0.length === 0){
+                        break;
+                    }
+                    var int_numOfRetry = parseInt((str_id_tmp.match(/_\d+$/))[0].replace(/^_/, '')) + 1;
+                    str_id_tmp = str_id_tmp.replace(/_\d+$/, `_${int_numOfRetry.toString()}`);
+                }
+                var str_xpath_assertElem = `/div[@id="${str_id_tmp}"]`
+
+                var objarr_expectedAsBody = await obj_webDriver.findElements(By.tagName('body'));
+                var obj_expectedAsBody = objarr_expectedAsBody[0];
+
+                var str_tmpElem = 
+`<div id="${str_id_tmp}" style="user-select: text; align-items: center; background: rgba(0, 0, 0, .75); bottom: 0; display: flex; justify-content: center; left: 0; position: fixed; right: 0; top: 0; color: #000000">
+    <div class="modal-container" style="background: #fff; border-radius: 4px; padding: 30px 20px; overflow: auto; 
+        /* 横幅の設定 */
+        width: 80%; 
+        max-width: 80%; 
+        /* 高さの設定 */
+        height: 80%;
+        max-height: 80%;
+        /* 中身を広げるための設定 */
+        display: flex;
+        flex-direction: column;">
+
+        <textarea style="box-sizing: border-box; width: 100%; height: 100%; flex-grow: 1; resize: none; background-color:#fff; color:#000000;
+            /* 横に長くても改行させない設定 */
+            white-space: nowrap; 
+            /* 横スクロールバーを必要に応じて表示 */
+            overflow: auto;""></textarea>
+    </div>
+</div>
+`
+                ;
+
+                await obj_webDriver.executeScript(
+                    "document.body.insertAdjacentHTML('beforeend', arguments[0]);", 
+                    str_tmpElem
+                );
+                var obj_elems_expectedAsTextArea = await obj_webDriver.findElements(By.xpath(`//body/div[@id="${str_id_tmp}"]//textarea`));
+                obj_elems_expectedAsTextArea = obj_elems_expectedAsTextArea[0];
+
+                // Click textarea
+                var int_timeOfTryClick = 0;
+                for(int_timeOfTryClick = 0 ; int_timeOfTryClick < int_tryTimesForClickTextEditor ; int_timeOfTryClick++){
+                    await obj_actions.clear();
+                    await obj_elems_expectedAsTextArea.click();
+                        //Note:
+                        // <div> 要素を上記 `.executeScript()` で追加しても、 
+                        // jsfiddle.net 側の既存の要素が追加した要素の更に手前に表示される。
+                        // `.move()` -> `.press()` -> `.release()` でクリックさせる場合に邪魔する可能性がありそうなので、
+                        // ここでは `.click()` を仕様
+                    var bl_isFocused = await obj_webDriver.executeScript(
+                        `return document.activeElement === arguments[0];`, 
+                        obj_elems_expectedAsTextArea
+                    );
+                    if(bl_isFocused){ // <textarea> にフォーカスされた = キャレットが表示された場合
+                        break;
+                    }
+                    console.log(`Caret not found. Retry...`);
+                }
+                if(int_timeOfTryClick >= int_tryTimesForClickTextEditor){
+                    let str_msg = `Caret not found`
+                    throw(new Error(str_msg, {
+                        details: {
+                            "argument":str_arg,
+                            "path":str_absPathOfArg,
+                            "result":"NG",
+                            "message":str_msg
+                        }
+                    })); //todo 受け取り側でハンドリング
+                }
+
+                // Paste to textarea
+                await obj_actions.clear();
+                obj_actions
+                    .keyDown(Key.CONTROL)
+                    .sendKeys('v')
+                    .keyUp(Key.CONTROL)
+                ;
+                await obj_actions.perform();
+
+                // 文字列が一致するかどうか確認
+                obj_elems_expectedAsTextArea = await obj_webDriver.findElements(By.xpath(`//body/div[@id="${str_id_tmp}"]//textarea`)); // 一応とりなおし
+                obj_elems_expectedAsTextArea = obj_elems_expectedAsTextArea[0];
+
+                var str_pasted = await obj_elems_expectedAsTextArea.getAttribute('value');
+
+                //todo str_pasted が違った場合の処理
+
+                // 追加した element を削除
+                await obj_webDriver.executeScript(
+                    `document.getElementById(arguments[0]).remove();`,
+                    str_id_tmp
+                );
+
+                // --------------------------------------------------------<ClipBoard 文字列を Selenium が意図通り引っ張ってこれるかどうか確認>
+            }
+
+            //
+            // `obj_webDriver.actions()` で ctrl+v の結果、clipboard の文字列が貼り付けられるかどうか確認
+            //
+            async function func_checkclipboard(str_expected){
+
+                var str_id_tmp = `${func_checkclipboard.name}_0`;
+                while(true){ // 存在しない id 名称を抽出するまで無限ループ
+                    var obj_elems_expectedAs0 = await obj_webDriver.findElements(By.id(str_id_tmp));
+                    if(obj_elems_expectedAs0.length === 0){
+                        break;
+                    }
+                    var int_numOfRetry = parseInt((str_id_tmp.match(/_\d+$/))[0].replace(/^_/, '')) + 1;
+                    str_id_tmp = str_id_tmp.replace(/_\d+$/, `_${int_numOfRetry.toString()}`);
+                }
+                var str_xpath_assertElem = `/div[@id="${str_id_tmp}"]`
+
+                var objarr_expectedAsBody = await obj_webDriver.findElements(By.tagName('body'));
+                var obj_expectedAsBody = objarr_expectedAsBody[0];
+
+                var str_tmpElem = 
+`<div id="${str_id_tmp}" style="user-select: text; align-items: center; background: rgba(0, 0, 0, .75); bottom: 0; display: flex; justify-content: center; left: 0; position: fixed; right: 0; top: 0; color: #000000">
+    <div class="modal-container" style="background: #fff; border-radius: 4px; padding: 30px 20px; overflow: auto; 
+        /* 横幅の設定 */
+        width: 80%; 
+        max-width: 80%; 
+        /* 高さの設定 */
+        height: 80%;
+        max-height: 80%;
+        /* 中身を広げるための設定 */
+        display: flex;
+        flex-direction: column;">
+
+        <textarea style="box-sizing: border-box; width: 100%; height: 100%; flex-grow: 1; resize: none; background-color:#fff; color:#000000;
+            /* 横に長くても改行させない設定 */
+            white-space: nowrap; 
+            /* 横スクロールバーを必要に応じて表示 */
+            overflow: auto;""></textarea>
+    </div>
+</div>
+`
+                ;
+
+                await obj_webDriver.executeScript(
+                    "document.body.insertAdjacentHTML('beforeend', arguments[0]);", 
+                    str_tmpElem
+                );
+                var obj_elems_expectedAsTextArea = await obj_webDriver.findElements(By.xpath(`//body/div[@id="${str_id_tmp}"]//textarea`));
+                obj_elems_expectedAsTextArea = obj_elems_expectedAsTextArea[0];
+
+                // Click textarea
+                for(let int_timeOfTryClick = 0 ; int_timeOfTryClick < int_tryTimesForClickTextEditor ; int_timeOfTryClick++){
+                    await obj_actions.clear();
+                    await obj_elems_expectedAsTextArea.click();
+                        //Note:
+                        // <div> 要素を上記 `.executeScript()` で追加しても、 
+                        // jsfiddle.net 側の既存の要素が追加した要素の更に手前に表示される。
+                        // `.move()` -> `.press()` -> `.release()` でクリックさせる場合に邪魔する可能性がありそうなので、
+                        // ここでは `.click()` を仕様
+                    var bl_isFocused = await obj_webDriver.executeScript(
+                        `return document.activeElement === arguments[0];`, 
+                        obj_elems_expectedAsTextArea
+                    );
+                    if(bl_isFocused){ // <textarea> にフォーカスされた = キャレットが表示された場合
+                        break;
+                    }
+                    console.log(`Caret not found. Retry...`);
+                }
+
+                // Paste to textarea
+                await obj_actions.clear();
+                obj_actions
+                    .keyDown(Key.CONTROL)
+                    .sendKeys('v')
+                    .keyUp(Key.CONTROL)
+                ;
+                await obj_actions.perform();
+
+                // 文字列が一致するかどうか確認
+                obj_elems_expectedAsTextArea = await obj_webDriver.findElements(By.xpath(`//body/div[@id="${str_id_tmp}"]//textarea`)); // 一応とりなおし
+                obj_elems_expectedAsTextArea = obj_elems_expectedAsTextArea[0];
+
+                var str_pasted = await obj_elems_expectedAsTextArea.getAttribute('value');
+
+                console.log(str_pasted===str_expected);
+
+                // 追加した element を削除
             }
 
         })();
